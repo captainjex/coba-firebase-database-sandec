1a65c7430fa94f8f1bd9fa7a44985d7878edd0a2
wkwksama <mailpakgasing@gmail.com>
2017-03-03 22:28:10 +0700

    initial. bisa nambah, hapus, ngubah data

---
+++ app/src/main/java/com/example/wkwksama/ojekonline/Order.java
@@ -0,0 +1,34 @@
+package com.example.wkwksama.ojekonline;
+
+/**
+ * Created by wkwksama on 2/3/17.
+ */
+
+public class Order {
+    public String dari, ke;
+    public int jarak;
+
+    public String getDari() {
+        return dari;
+    }
+
+    public void setDari(String dari) {
+        this.dari = dari;
+    }
+
+    public String getKe() {
+        return ke;
+    }
+
+    public void setKe(String ke) {
+        this.ke = ke;
+    }
+
+    public int getJarak() {
+        return jarak;
+    }
+
+    public void setJarak(int jarak) {
+        this.jarak = jarak;
+    }
+}
